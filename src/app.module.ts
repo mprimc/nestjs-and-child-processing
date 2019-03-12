36efd33524b76201cc1ad248ee66ad8d3fa37c13
Miha Primc <miha.primc@beenius.tv>
2019-03-12 09:27:10 +0100

    Chore: remove default Hello World example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CatModule } from './cats/cats.module';
 import { DbModule } from './db/db.module';
 
 @Module({
   imports: [CatModule, DbModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
